19559174c2f3ffc7c9779c73837e037894fc0d1a
Jacob Lin <lovetaeyeon507@gmail.com>
2024-03-04 22:47:25 +0800

    Add linux list sort implementation
    The original linux `list_sort()` is added to `queue.c`. Some linux
    specific info and compiler hints were removed. A new command,
    `do_linux_list_sort` is added to qtest. Similar to `do_qsort`, order
    check to the queue is performed after the sort and will report error if
    violate the rules.

--- queue.c
+++ queue.c
@@ -448,3 +448,135 @@ void q_shuffle(struct list_head *head)
             curr = next_temp;
     }
 }
+
+static struct list_head *merge(bool (*cmp)(struct list_head *,
+                                           struct list_head *,
+                                           bool),
+                               struct list_head *a,
+                               struct list_head *b,
+                               bool descend)
+{
+    struct list_head *head = NULL, **tail = &head;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (!cmp(a, b, descend)) {
+            *tail = a;
+            tail = &a->next;
+            a = a->next;
+            if (!a) {
+                *tail = b;
+                break;
+            }
+        } else {
+            *tail = b;
+            tail = &b->next;
+            b = b->next;
+            if (!b) {
+                *tail = a;
+                break;
+            }
+        }
+    }
+    return head;
+}
+
+static void merge_final(bool (*cmp)(struct list_head *,
+                                    struct list_head *,
+                                    bool),
+                        struct list_head *head,
+                        struct list_head *a,
+                        struct list_head *b,
+                        bool descend)
+{
+    struct list_head *tail = head;
+    size_t count = 0;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (!cmp(a, b, descend)) {
+            tail->next = a;
+            a->prev = tail;
+            tail = a;
+            a = a->next;
+            if (!a)
+                break;
+        } else {
+            tail->next = b;
+            b->prev = tail;
+            tail = b;
+            b = b->next;
+            if (!b) {
+                b = a;
+                break;
+            }
+        }
+    }
+
+    /* Finish linking remainder of list b on to tail */
+    tail->next = b;
+    do {
+        if (!++count)
+            cmp(b, b, descend);
+        b->prev = tail;
+        tail = b;
+        b = b->next;
+    } while (b);
+
+    /* And the final links to make a circular doubly-linked list */
+    tail->next = head;
+    head->prev = tail;
+}
+
+void list_sort(struct list_head *head,
+               bool (*cmp)(struct list_head *, struct list_head *, bool),
+               bool descend)
+{
+    struct list_head *list = head->next, *pending = NULL;
+    size_t count = 0; /* Count of pending */
+
+    if (list == head->prev) /* Zero or one elements */
+        return;
+
+    /* Convert to a null-terminated singly-linked list. */
+    head->prev->next = NULL;
+
+    do {
+        size_t bits;
+        struct list_head **tail = &pending;
+
+        /* Find the least-significant clear bit in count */
+        for (bits = count; bits & 1; bits >>= 1)
+            tail = &(*tail)->prev;
+        /* Do the indicated merge */
+        if (bits) {
+            struct list_head *a = *tail, *b = a->prev;
+
+            a = merge(cmp, b, a, descend);
+            /* Install the merged result in place of the inputs */
+            a->prev = b->prev;
+            *tail = a;
+        }
+
+        /* Move one element from input list to pending */
+        list->prev = pending;
+        pending = list;
+        list = list->next;
+        pending->next = NULL;
+        count++;
+    } while (list);
+
+    /* End of input; merge together all the pending lists. */
+    list = pending;
+    pending = pending->prev;
+    for (;;) {
+        struct list_head *next = pending->prev;
+
+        if (!next)
+            break;
+        list = merge(cmp, pending, list, descend);
+        pending = next;
+    }
+    /* The final merge, rebuilding prev links */
+    merge_final(cmp, head, pending, list, descend);
+}
